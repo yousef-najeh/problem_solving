9262070ac152cd4fb48647b67354e83f6f5a4aff
yousef samara <92521652+yousefQJ19@users.noreply.github.com>
2024-10-03 20:30:25 +0300

    Time: 52 ms (26.06%) | Memory: 51.1 MB (41.68%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+bool canJump(vector<int>& nums) {
+    int maxReach = 0; 
+    int n = nums.size();
+
+    for (int i = 0; i < n; i++) {
+        if (maxReach < i) return false;
+        maxReach = max(maxReach, i + nums[i]); 
+    }
+
+    return true;
+}
+
+};
