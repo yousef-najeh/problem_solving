7a5a5ad4b61c8749a40fd1269f7a9c7146f53e84
yousef samara <92521652+yousefQJ19@users.noreply.github.com>
2024-10-06 01:28:49 +0300

    Time: 71 ms (60.71%) | Memory: 22.6 MB (54.00%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
     bool closeStrings(std::string word1, std::string word2) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         if (word1.size() != word2.size()) return false;
-        
         vector<int> counts1(26, 0), counts2(26, 0);
         set<char> set1, set2;
         
