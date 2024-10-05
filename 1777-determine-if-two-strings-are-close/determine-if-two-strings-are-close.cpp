1c491ef0cbb1a6785410343bd81ae732098532a1
yousef samara <92521652+yousefQJ19@users.noreply.github.com>
2024-10-06 01:18:09 +0300

    Time: 88 ms (55.15%) | Memory: 22.4 MB (57.02%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
@@ -3,8 +3,8 @@ public:
     bool closeStrings(std::string word1, std::string word2) {
         if (word1.size() != word2.size()) return false;
         
-        std::vector<int> counts1(26, 0), counts2(26, 0);
-        std::set<char> set1, set2;
+        vector<int> counts1(26, 0), counts2(26, 0);
+        set<char> set1, set2;
         
         for (char c : word1) {
             counts1[c - 'a']++;
@@ -16,11 +16,11 @@ public:
             set2.insert(c);
         }
         
-        // Sort the frequency counts
+        
         std::sort(counts1.begin(), counts1.end());
         std::sort(counts2.begin(), counts2.end());
         
-        // Check if both sets of characters and their frequency counts match
+       
         return set1 == set2 && counts1 == counts2;
     }
 };
