4e0ff676ddb8de055ca4d833c249e8f5a030673e
yousef samara <92521652+yousefQJ19@users.noreply.github.com>
2024-10-06 04:32:34 +0300

    Time: 119 ms (62.25%) | Memory: 76.8 MB (11.90%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int  longestConsecutive(vector<int>& nums) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         if (nums.size() == 0)return 0;
         if (nums.size() == 1)return 1;
         sort(nums.begin(), nums.end());
